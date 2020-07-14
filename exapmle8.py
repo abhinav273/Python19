8426926851ceeceafcb487301313ea7c2e81fbd0
Code With Abhinav <67705141+abhinav273@users.noreply.github.com>
2020-07-14 23:47:02 +0530

    Add files via upload

---
+++ exapmle8.py
@@ -0,0 +1,25 @@
+fname = input('Enter file name: ')
+hand = open(fname)
+
+lst = list()
+
+for line in hand:
+    if line.startswith('From:'):
+        line = line.split()
+        lst.append(line[1])
+
+counts = dict()
+for word in lst:
+    counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+
+for word,count in counts.items():
+    if bigcount == None or count>bigcount:
+        bigcount = count
+        bigword = word
+
+print(bigword,bigcount)
+
+# print(counts)
